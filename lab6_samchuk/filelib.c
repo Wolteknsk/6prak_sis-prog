95ebad91dd0bb735e9ef39cf4ff4fd713d9bc1c9
Wolteknsk <andsam913@gmail.com>
2025-11-06 10:31:59 +0000

    afs

---
+++ lab6_samchuk/filelib.c
@@ -0,0 +1,9 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+void save_message(const char *filename, const char *message) {
+    int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    write(fd, message, strlen(message));
+    close(fd);
+}
